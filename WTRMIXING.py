fe89b5457e262d44e53de15d25126afd29008f98
Miloni Shah <89334548+miloni03@users.noreply.github.com>
2023-03-13 21:05:52 +0530

    Add files via upload

---
+++ WTRMIXING.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for i in range(int(input())):
+    a,b,c,d=map(int,input().split())
+    if(a<=b):
+        if(b-a<=c):
+            print("Yes")
+        else:
+            print("No")
+    else:
+        if(a-b<=d):
+            print("Yes")
+        else:
+            print("No")
